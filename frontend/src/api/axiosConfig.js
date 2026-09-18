b0d7b3946deec0264a6247ceac7b311ae79de12f
Alok Kumar Dangi <alokdangi2004@gmail.com>
2026-09-18 12:11:28 +0530

    updated code

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://paytm-0cwf.onrender.com'
+    baseURL: 'https://paytm-0cwf.onrender.com/api'
 });
 
 export default api;
