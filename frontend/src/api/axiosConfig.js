c5f02c122c1538f4b879ba28c0dd726a07baaf15
Alok Kumar Dangi <alokdangi2004@gmail.com>
2026-09-18 12:10:10 +0530

    updated code

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,16 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api', // Aapka backend URL
-});
-
-// Interceptor jo automatically localStorage se token utha kar bhej dega
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+    baseURL: 'https://paytm-0cwf.onrender.com'
 });
 
 export default api;
